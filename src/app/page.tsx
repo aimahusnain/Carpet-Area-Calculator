50ab6a45d9ef1a15b1f565e99ae09c091e171d3a
Muhammad Husnain <aimahusnain@gmail.com>
2024-11-25 20:44:45 +0500

    Added Requirements image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import CarpetCalculator from '@/components/carpet-calculator'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Carpet Calculator</h1>
       <CarpetCalculator />
+      <div>
+        <Image src="/Prposed Corrections_Page1.jpg" alt="Requirements Image" width={1000} height={1500} />
+      </div>
     </main>
   )
 }
